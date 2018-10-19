98740833fec5bc709b4afa0a7fda7f5dc169b29b
Azamat Tadeev <azamat.tadeev@mail.ru>
2018-10-19 14:29:27 +0300

    Написан код для ControllerServlet

--- src/main/java/ControllerServlet.java
+++ src/main/java/ControllerServlet.java
@@ -1,7 +1,51 @@
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ControllerServlet extends HttpServlet {
+    private ServletContext context;
 
+    @Override
+    public void init() throws ServletException {
+        context = getServletContext();
+        super.init();
+    }
 
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp){
+
+        String yString = req.getParameter("yValue");
+        String xString = req.getParameter("xValue");
+        String rString = req.getParameter("rValue");
+        if (xString == null || yString == null || rString == null) {
+            redirectToForm(resp);
+        }else {
+            try {
+                Integer.parseInt(xString);
+                Integer.parseInt(rString);
+                Double.parseDouble(yString);
+            } catch (Exception ex) {
+                redirectToForm(resp);
+            }
+            try {
+                context.getRequestDispatcher("/WEB-INF/pages/result.jsp").forward(req, resp);
+            }catch (ServletException sex){
+                context.log(sex.getMessage());
+            }catch (IOException ioex){
+                context.log(ioex.getMessage());
+            }
+        }
+    }
+
+    private void redirectToForm(HttpServletResponse resp){
+        try {
+            resp.sendRedirect("form.jsp");
+        }catch (IOException ioex){
+            context.log(ioex.getMessage());
+        }
+    }
 
 }
